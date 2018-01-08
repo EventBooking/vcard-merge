a7248e1ab72f21ea7d40fc6ae7fe893fa12b65aa
Rob Scott <rob@eventbooking.com>
2018-01-08 14:52:20 -0500

    Read with one library, write with the other. Very bizarre.

---
+++ vcard-merge/Helpers/Converter.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using MixERP.Net.VCards.Models;
+using MixERP.Net.VCards.Types;
+using vCards;
+
+namespace VCardMerge.Helpers
+{
+    public static class Converter
+    {
+        public static string ToString( StringCollection names )
+        {
+            //var text = string.Join( ',', names );
+            var text = new StringBuilder();
+            foreach (var x in names)
+            {
+                text.Append( x );
+                text.Append( " " );
+            }
+            return text.ToString().Trim();
+        }
+
+        public static string[] ToArray( StringCollection things )
+        {
+            return things.Cast<string>().ToArray();
+        }
+
+        public static Email ConvertEmail( vCardEmailAddress x )
+        {
+            var model = new Email
+            {
+                EmailAddress = x.Address,
+                Type = EmailType.Smtp
+            };
+            return model;
+        }
+
+        public static Address ConvertAddress( vCardDeliveryAddress x )
+        {
+            var model = new Address
+            {
+                Type = ConvertAddressType( x.AddressType ),
+                Street = x.Street,
+                Country = x.Country,
+                ExtendedAddress = string.Empty,
+                Label = string.Empty,
+                Locality = x.City,
+                PoBox = string.Empty,
+                PostalCode = x.PostalCode,
+                Region = x.Region,
+                Latitude = null,
+                Longitude = null,
+                Preference = 0,
+                TimeZone = TimeZoneInfo.Local
+            };
+            return model;
+        }
+
+        public static Telephone ConvertPhone( vCardPhone x )
+        {
+            var model = new Telephone
+            {
+                Type = ConvertPhoneType( x.PhoneType ),
+                Number = x.FullNumber
+            };
+            return model;
+        }
+
+        public static TelephoneType ConvertPhoneType( vCardPhoneTypes type )
+        {
+            switch (type)
+            {
+                case vCardPhoneTypes.BBS:
+                    return TelephoneType.Bbs;
+
+                case vCardPhoneTypes.Car:
+                    return TelephoneType.Car;
+
+                case vCardPhoneTypes.Cellular:
+                case vCardPhoneTypes.CellularVoice:
+                    return TelephoneType.Cell;
+
+                case vCardPhoneTypes.Fax:
+                    return TelephoneType.Fax;
+
+                case vCardPhoneTypes.Home:
+                    return TelephoneType.Home;
+
+                case vCardPhoneTypes.ISDN:
+                    return TelephoneType.Isdn;
+
+                case vCardPhoneTypes.Work:
+                case vCardPhoneTypes.WorkVoice:
+                    return TelephoneType.Work;
+
+                case vCardPhoneTypes.WorkFax:
+                    return TelephoneType.Fax;
+
+                case vCardPhoneTypes.Voice:
+                    return TelephoneType.Voice;
+
+                default:
+                    return TelephoneType.Work;
+            }
+        }
+
+        public static AddressType ConvertAddressType( vCardDeliveryAddressTypes type )
+        {
+            switch (type)
+            {
+                case vCardDeliveryAddressTypes.Domestic:
+                    return AddressType.Domestic;
+
+                case vCardDeliveryAddressTypes.Home:
+                    return AddressType.Home;
+
+                case vCardDeliveryAddressTypes.International:
+                    return AddressType.International;
+
+                case vCardDeliveryAddressTypes.Parcel:
+                    return AddressType.Parcel;
+
+                case vCardDeliveryAddressTypes.Postal:
+                    return AddressType.Postal;
+
+                case vCardDeliveryAddressTypes.Work:
+                    return AddressType.Work;
+
+                default:
+                    return AddressType.Work;
+            }
+        }
+
+        public static ClassificationType ConvertClass( vCardAccessClassification c )
+        {
+            switch (c)
+            {
+                case vCardAccessClassification.Confidential:
+                    return ClassificationType.Confidential;
+
+                case vCardAccessClassification.Private:
+                    return ClassificationType.Private;
+
+                case vCardAccessClassification.Public:
+                    return ClassificationType.Public;
+
+                default:
+                    return ClassificationType.Public;
+            }
+        }
+
+        public static Uri ConvertSource( vCardSourceCollection sources )
+        {
+            var src = sources.FirstOrDefault();
+            return src?.Uri;
+        }
+    }
+}
--- vcard-merge/Program.cs
+++ vcard-merge/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using CommandLineSwitchParser;
+using MixERP.Net.VCards;
+using MixERP.Net.VCards.Serializer;
+using MixERP.Net.VCards.Types;
 using vCards;
+using VCardMerge.Helpers;
 
-namespace vcard_merge
+namespace VCardMerge
 {
     public class Program
     {
@@ -26,10 +31,9 @@ namespace vcard_merge
         {
             if (!Directory.Exists( inputDir ))
                 throw new ArgumentException( "inputDir" );
-
             Encoding.RegisterProvider( CodePagesEncodingProvider.Instance );
 
-            var all = new List<vCard>();
+            var incoming = new List<vCard>();
 
             var inputFiles = Directory.GetFiles( inputDir );
             foreach (var file in inputFiles)
@@ -38,21 +42,120 @@ namespace vcard_merge
                 if (fi.Extension != ".vcf")
                     continue;
                 var content = File.ReadAllText( fi.FullName );
-                TextReader tr = new StringReader( content );
+                var tr = new StringReader( content );
                 var rdr = new vCardStandardReader();
                 var card = rdr.Read( tr );
-                all.Add( card );
+                incoming.Add( card );
             }
 
-            using (TextWriter writer = File.CreateText( outputfile ))
+            var outgoing = new List<VCard>();
+            foreach (var inc in incoming)
             {
-                var wr = new vCardStandardWriter();
-                foreach (var c in all)
+                var card = new VCard
                 {
-                    wr.Write( c, writer, Encoding.UTF8.WebName );
-                }
+                    Version = VCardVersion.V3,
+                    UniqueIdentifier = inc.UniqueId,
+                    FirstName = inc.GivenName,
+                    LastName = inc.FamilyName,
+                    NickName = Converter.ToString( inc.Nicknames ),
+                    Prefix = inc.NamePrefix,
+                    Suffix = inc.NameSuffix,
+                    FormattedName = inc.DisplayName,
+                    Organization = inc.Organization,
+                    Title = inc.Title,
+                    Telephones = inc.Phones.Select( Converter.ConvertPhone ),
+                    Addresses = inc.DeliveryAddresses.Select( Converter.ConvertAddress ),
+                    Emails = inc.EmailAddresses.Select( Converter.ConvertEmail ),
+                    Categories = Converter.ToArray( inc.Categories ),
+                    Classification = Converter.ConvertClass( inc.AccessClassification ),
+                    Source = Converter.ConvertSource( inc.Sources ),
+
+                    Anniversary = null,
+                    BirthDay = null,
+                    Longitude = 0,
+                    Latitude = 0,
+                    MiddleName = string.Empty,
+                    TimeZone = null,
+                    Photo = null,
+                    CalendarAddresses = null,
+                    CalendarUserAddresses = null,
+                    CustomExtensions = null,
+                    DeliveryAddress = null,
+                    Gender = Gender.Unknown,
+                    Impps = null,
+                    Key = string.Empty,
+                    Kind = Kind.Individual,
+                    Languages = null,
+                    LastRevision = null,
+                    Logo = null,
+                    Mailer = string.Empty,
+                    Note = string.Empty,
+                    OrganizationalUnit = string.Empty,
+                    Relations = null,
+                    Role = inc.Role,
+                    SortString = string.Empty,
+                    Sound = string.Empty
+                };
+                outgoing.Add( card );
             }
+
+            var serialized = outgoing.Serialize();
+            File.WriteAllText( outputfile, serialized );
         }
+
+
+        //private static void Merge1( string inputDir, string outputfile )
+        //{
+        //    if (!Directory.Exists( inputDir ))
+        //        throw new ArgumentException( "inputDir" );
+
+        //    Encoding.RegisterProvider( CodePagesEncodingProvider.Instance );
+
+        //    var all = new List<vCard>();
+
+        //    var inputFiles = Directory.GetFiles( inputDir );
+        //    foreach (var file in inputFiles)
+        //    {
+        //        var fi = new FileInfo( file );
+        //        if (fi.Extension != ".vcf")
+        //            continue;
+        //        var content = File.ReadAllText( fi.FullName );
+        //        var tr = new StringReader( content );
+        //        var rdr = new vCardStandardReader();
+        //        var card = rdr.Read( tr );
+        //        all.Add( card );
+        //    }
+
+        //    using (TextWriter writer = File.CreateText( outputfile ))
+        //    {
+        //        var wr = new vCardStandardWriter();
+        //        foreach (var c in all)
+        //        {
+        //            wr.Write( c, writer, Encoding.UTF8.WebName );
+        //        }
+        //    }
+        //}
+
+        //private static void Merge2( string inputDir, string outputfile )
+        //{
+        //    if (!Directory.Exists( inputDir ))
+        //        throw new ArgumentException( "inputDir" );
+
+        //    var all = new List<VCard>();
+
+        //    var inputFiles = Directory.GetFiles( inputDir );
+        //    foreach (var file in inputFiles)
+        //    {
+        //        var fi = new FileInfo( file );
+        //        if (fi.Extension != ".vcf")
+        //            continue;
+        //        var vcards = Deserializer.Deserialize( fi.FullName );
+        //        all.AddRange( vcards );
+        //    }
+
+        //    var serialized = all.Serialize();
+        //    File.WriteAllText( outputfile, serialized );
+        //}
     }
 
     public class Options
